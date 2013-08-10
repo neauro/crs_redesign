41c5e0f7ed06c6783082b39eb09e4790e4fa8ff8
nadine <neauro@gmail.com>
2013-08-10 11:25:29 -0700

    making the boxes

--- index.php
+++ index.php
@@ -8,12 +8,22 @@
 <article>
 <!--    <ul><img src="_images/home.gif"/></ul> -->
     
+<!--
     <h2><img src="_images/home_small.gif"/>  What do you want:</h2>
     <a href="emr.php"><ul><img src="_images/patient_small.gif"/>  Patient Record Management</ul></a>
     <a href="cashflow.php"><ul><img src="_images/cash_flow_small.gif"/>  Help Increase Cashflow</ul></a>
     <a href="/coding.php"><ul><img src="_images/paperwork_small.gif"/>  Insurer Claims Submissions and Acceptence</ul></a>
     <a href="/payments.php"><ul><img src="_images/payment_small.gif"/>  Payments Received within a Timley Mannor</ul></a>
     <a href="/credentialling.php"><ul><img src="_images/compliance_small.gif"/>  Confirm Compliance with New Government Regulations</ul></a>
+-->
+<div id="icons">
+  <div class="menu-bit"><a href="emr.php"></a><p>Patient Record Management</p></a></div>
+  <div class="menu-bit middle"><a href="cashflow.php"></a><p>Cashflow</p></a></div>
+  <div class="menu-bit"><a href="coding.php"></a><p>Insurer Claims</p></a></div>
+  <div class="menu-bit"><a href="payments.php"></a><p>Payments</p></a></div>
+  <div class="menu-bit middle"><a href="credentialling.php"></a><p>Government Regulation Compliance</p></a></div>
+  <div class="menu-bit"><a href="analysis.php"></a><p>Practice Analysis</p></a></div>
+</div>
 
 
     <h3>Who are we?</h3>
