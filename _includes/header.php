1af01b512a1f2eb12b62e738b5a90ccda6b18c53
nadine <neauro@gmail.com>
2013-08-13 19:40:47 -0700

    yay header

--- _includes/header.php
+++ _includes/header.php
@@ -11,46 +11,45 @@
 <body>
 
 <!-- Begin Page Wrapper -->
-<div id="page">
-
-  <div id="header">
-    <div class="contents">
-      <a href="/"><h1>Comprehensive<br/>Revenue<br/>Solutions</h1></a>
-      <div id="contact-header">
-        <p>
-          Contact us to schedule a free on-site consultation.
-        </p>
-        <ul>
-          <li><strong>Call: </strong> <a href="">(888) 888-8888</a></li>
-          <li><strong>Email: </strong> <a href="">trichards@comprevenuesolutions.com</a></li>
-        </ul>
-      </div>
+<div id="header">
+  <div class="contents">
+    <a href="/"><h1>Comprehensive<br/>Revenue<br/>Solutions</h1></a>
+    <div id="contact-header">
+      <p>
+        Contact us to schedule a free on-site consultation.
+      </p>
+      <ul>
+        <li><strong>Call: </strong> <a href="">(888) 888-8888</a></li>
+        <li><strong>Email: </strong> <a href="">trichards@comprevenuesolutions.com</a></li>
+      </ul>
     </div>
   </div>
+</div>
 
-	<div class="menu">
-		<ul>
-			<li><a href="/index.php" >Home</a></li>
-			<li><span>Services</span>
-				<ul>
-					<li><a href="/emr.php">EMR / EHR</a></li>
-					<li><a href="/cashflow.php">Practice Analysis</a></li>
-					<li><a href="/coding.php">Patient Payments</a></li>
-					<li><a href="/payments.php">Billing Services</a></li>
-					<li><a href="/credentialling.php">Credentialling</a></li>
-					<li><a href="/credentialling.php">Compliance</a></li>
-					<li><a href="/faq.php">FAQ</a></li>
-			   </ul>
-		  </li>
-			<li><span>About Us</span>
-              <ul>
-                <li><a href="/credentials
+<div class="menu">
+  <ul>
+    <li><a href="/index.php" >Home</a></li>
+    <li><span>Services</span>
+      <ul>
+        <li><a href="/emr.php">EMR / EHR</a></li>
+        <li><a href="/cashflow.php">Practice Analysis</a></li>
+        <li><a href="/coding.php">Patient Payments</a></li>
+        <li><a href="/payments.php">Billing Services</a></li>
+        <li><a href="/credentialling.php">Credentialling</a></li>
+        <li><a href="/credentialling.php">Compliance</a></li>
+        <li><a href="/faq.php">FAQ</a></li>
+       </ul>
+    </li>
+    <li><span>About Us</span>
+            <ul>
+              <li><a href="/credentials
 .php">Credentials</a></li>
-                <li><a href="/testimonials.php">Testimonials</a></li>
-                <li><a href="/location.php">Location</a></li>
-                <li><a href="/experience.php">Tanya's Experience</a></li>
-              </ul>
-          </li>
-			<li><a href="/contact.php">Contact Us</a></li>
-		</ul>
-	</div>
+              <li><a href="/testimonials.php">Testimonials</a></li>
+              <li><a href="/location.php">Location</a></li>
+              <li><a href="/experience.php">Tanya's Experience</a></li>
+            </ul>
+        </li>
+    <li><a href="/contact.php">Contact Us</a></li>
+  </ul>
+</div>
+<div id="page">
