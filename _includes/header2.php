8c5ac0f503cdd0684cadaef3f180f45ad2b0b25b
nadine <neauro@gmail.com>
2013-08-18 23:45:09 -0700

    version 2

---
+++ _includes/header2.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0" />
+  <link href="styles2.css" rel="stylesheet" type="text/css" media="all" />
+  <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:700,400' rel='stylesheet' type='text/css'>
+  <title>Comprehensive Revenue Solutions</title>
+</head>
+
+<body>
+
+<!-- Begin Page Wrapper -->
+<div id="header">
+  <div class="contents">
+    <!-- <a href="/"><h1>Comprehensive<br/>Revenue<br/>Solutions</h1></a> -->
+    <a href="/"><h1><span>Comprehensive </span><span>Revenue Solutions</span></h1></a>
+    <h2>A medical billing and cash flow consulting company</h2>
+    <div id="contact-header">
+      <p>
+        Schedule a free on-site consultation.
+      </p>
+      <ul>
+        <li><a href=""><img src="_images/telephone.png" alt="Call us at this phone number: "/> (888) 888-8888</a></li>
+        <li><a href=""><img id="envelope" src="_images/envelope.png" alt="Or e-mail us at:"/>trichards@comprevenuesolutions.com</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div class="menu">
+  <ul>
+    <li><a href="/index.php" >Home</a></li>
+    <li><span>Our Services</span>
+      <ul>
+        <li><a href="/services.php?tabindex=0">Billing Services</a></li>
+        <li><a href="/services.php?tabindex=1">Patient Payments</a></li>
+        <li><a href="/services.php?tabindex=2">Cash Flow Management</a></li>
+        <li><a href="/services.php?tabindex=3">Document Management</a></li>
+        <li><a href="/services.php?tabindex=4">EMR / EHR</a></li>
+        <li><a href="/services.php?tabindex=5">Auditing Services</a></li>
+        <li><a href="/services.php?tabindex=6">Credentialling</a></li>
+        <li><a href="/faq.php">FAQ</a></li>
+       </ul>
+    </li>
+    <li><span>About Us</span>
+            <ul>
+              <li><a href="/credentials
+.php">Credentials</a></li>
+              <li><a href="/testimonials.php">Testimonials</a></li>
+              <li><a href="/location.php">Location</a></li>
+              <li><a href="/experience.php">Tanya's Experience</a></li>
+            </ul>
+        </li>
+    <li><a href="/contact.php">Contact Us</a></li>
+  </ul>
+</div>
+<div id="page">
