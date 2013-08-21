44867fa79a54178de439b1aad6037b47fc690ec6
Kate <kateechka@d-69-91-181-152.dhcp4.washington.edu>
2013-08-20 18:55:36 -0700

    Services text tweaks

--- services.php
+++ services.php
@@ -20,7 +20,6 @@
                 <div>
                     <h1>Electronic Medical Billing Services</h1>
                     <h2>Collecting on Insurer Claims</h2>
-                    <p>A web-based billing management service will help increase your cash flow and improve your bottom line. But managing your own in-house insurance and reimbursement collection process can be costly, demanding hours of valuable staff time.  Spend less time the processing of claims, and more time caring for patients and building your practice.</p>
                     <p>Benefits of choosing our Billing Service:
                         <ul class="tabContent">
                     <li>No Software Purchase Necessary; no upgrade, install, license or administration fees to benefit from real-time system upgrades and access to costly technology without the associated costs.</li>
@@ -32,13 +31,12 @@
                     <li>Greater Data Security; remove the worry of losing critical patient billing data housed locally on your hard drive by storing data on our secure servers located in a protected location</li>
                     </ul>
                     </p>
+                    <p>A web-based billing management service will help increase your cash flow and improve your bottom line. But managing your own in-house insurance and reimbursement collection process can be costly, demanding hours of valuable staff time.  Spend less time the processing of claims, and more time caring for patients and building your practice.</p>
+                    
                 </div>
                 <div>
                 <h1>Patient Payment System</h1>
                 <h2>Outstanding Patient Balances</h2>
-                <p>
-                    Using the web-based software, FirstPaid, we calculate, process, and quickly collect your patient’s payments in a cost-efficient manner. Designed to simplify and accelerate the revenue cycle process, FirstPaid calculates out-of-pocket patients costs, schedules payments, and automates recurring payments on a secure platform. 
-                </p>
                 <p>
                     Benefits of choosing our Patient Payment Service:
                     <ul class="tabContent">
@@ -50,15 +48,14 @@
                 </ul>
                 </p>
 
+                <p>
+                    Using the web-based software, FirstPaid, we calculate, process, and quickly collect your patient’s payments in a cost-efficient manner. Designed to simplify and accelerate the revenue cycle process, FirstPaid calculates out-of-pocket patients costs, schedules payments, and automates recurring payments on a secure platform. 
+                </p>
+                
                 </div>
                 <div >
                     <h1>Accounts Receivable Management</h1>
                     <h2>Noting Past-Due Accounts</h2>
-                    <p>Lingering follow-up on past due accounts is one major issues when managing account receivables. When account is uncollectable it is a costly transition to a collection agency. We are here to help. 
-                    </p>
-                    <p>
-                        We use a time-tested, automated recovery process that gently engages with your indebted patient or client before a more direct approach is required. Recover more due accounts quickly and with much  less risk to your valuable patient relationships. You maintain complete control of the process with a detailed daily report through the web-based MaxCollect™ system, and direct contact once your patient initiates payment. Preserve your patient's goodwill and reclaim the profit that is rightfully yours.
-                    </p>
                     <p>
                         Benefits of choosing our Accounts Receivable Management:
                         <ul class="tabContent">
@@ -72,14 +69,19 @@
                             <li>Low cost, patient friendly solution, not a collection agency</li>
                         </ul>
                     </p>
+                    <p>Lingering follow-up on past due accounts is one major issues when managing account receivables. When account is uncollectable it is a costly transition to a collection agency. We are here to help. 
+                    </p>
+                        
+                    <p>
+                        We use a time-tested, automated recovery process that gently engages with your indebted patient or client before a more direct approach is required. Recover more due accounts quickly and with much  less risk to your valuable patient relationships. You maintain complete control of the process with a detailed daily report through the web-based MaxCollect™ system, and direct contact once your patient initiates payment. Preserve your patient's goodwill and reclaim the profit that is rightfully yours.
+                    </p>
+                    <p>
+                    
 
                 </div>
                 <div>
                     <h1>Document Management Services</h1>
                     <h2>Converting to Electronic Docs</h2>
-                    <p>Is your staff overwhelmed managing and retrieving paper documents? 
-The path toward a paperless office begins with our web-based document management tool, iDocsNOW. The web-based tool converts your paper documents into electronic images; simply scan paper files then log into your secure electronic file cabinet to create a fully-searchable electronic filing cabinet. Reduce the amount of paper you buy and eliminate postage, photocopies, fax machines, couriers and off-site storage.
-                    </p>
                     <p>
                         Benefits of choosing our Document Management Service :
                     <ul class="tabContent">
@@ -90,13 +92,15 @@ The path toward a paperless office begins with our web-based document management
                     <li>iDocsNOW will help you meet federal privacy and HIPAA security regulations</li>
 
                     </p>
+                    <p>Is your staff overwhelmed managing and retrieving paper documents? 
+The path toward a paperless office begins with our web-based document management tool, iDocsNOW. The web-based tool converts your paper documents into electronic images; simply scan paper files then log into your secure electronic file cabinet to create a fully-searchable electronic filing cabinet. Reduce the amount of paper you buy and eliminate postage, photocopies, fax machines, couriers and off-site storage.
+                    </p>
+                    
                 </div>
                 <div>
                     
                       <h1> EMR / EHR</h1>
                       <h2>We solve your Medical Records Workflow Trauma.</h2>
-                    <p>Organizing and keeping track of Medical Records can be a daunting task.  Converting your records to the MediTouch EHR® System, tailored to your office’s existing workflow, will allow you to find records and track them through the entire process from patient visit through insurance payout.
-                    </p>
                     <p >
                       Benefits of choosing our Medical Records System:
                     <ul class="tabContent">
@@ -109,13 +113,14 @@ The path toward a paperless office begins with our web-based document management
                     <li>Increased Data Security</li>
                     <li>e-Prescribing, Labs and other common features</li>
                   </ul>
-                  
+                  </p>
+                    <p>Organizing and keeping track of Medical Records can be a daunting task.  Converting your records to the MediTouch EHR® System, tailored to your office’s existing workflow, will allow you to find records and track them through the entire process from patient visit through insurance payout.
+                    </p>
+                    
                 </div>
                 <div>
                     <h1> Auditing Services</h1>
                     <h2> We Code Claims for Success </h2>
-                    <p>Deploying Certified Coders on the Front End of the Billing Process Results in fewer Denials by Medicare and Private Insurance Companies. Our Coders are certified in AAPC and HIPAA and have the experience to decrease denials and speedup payouts from the insurance companies.
-                    </p>
                     <p>Benefits of choosing our Medical Coding Professionals
                     <ul class="tabContent">
                     <li>AAPC and HIPAA Certified</li>
@@ -126,11 +131,15 @@ The path toward a paperless office begins with our web-based document management
                     <li>Experience-Certified Coders for over 200 Specialties and knowledge to Easily Transition to ICD-10</li>
                     <li>With RAC Audits becoming more Frequent it is Important to keep Compliant and use our RAC Certified / Experienced Professionals</li>
                     </ul></p>
+                    <p>Deploying Certified Coders on the Front End of the Billing Process Results in fewer Denials by Medicare and Private Insurance Companies. Our Coders are certified in AAPC and HIPAA and have the experience to decrease denials and speedup payouts from the insurance companies.
+                    </p>
+                    
                 </div>
                 <div>
                     <h1>Credentialing Services</h1>
-                    <h2>Benefits of choosing our Credentialing Services</h2>
-                    <p>Our credentialing experts have over thirteen years of experience and provide a range of services from completing, maintaining and monitoring your licenses, registrations and certificates.
+                   
+                    
+                    <p>Benefits of choosing our Credentialing Services</p>
                    <ul class="tabContent">
                     <li>Confidentiality and Security of personal data</li>
                     <li>Cost savings when compared to staffing, wages and benefits</li>
@@ -143,6 +152,7 @@ The path toward a paperless office begins with our web-based document management
                     <li>Hospital Privileging</li>
                     <li>Choice of one time fees or annual services.</li>
                   </ul>
+                  <p>Our credentialing experts have over thirteen years of experience and provide a range of services from completing, maintaining and monitoring your licenses, registrations and certificates.</p>
                 </div>
             </div>
         </div>
